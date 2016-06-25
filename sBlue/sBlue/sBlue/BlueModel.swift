f1281261efe049d1db9118b6d26fe871d31a06c8
José Cañizares <jose.canizares@colorado.edu>
2016-06-24 19:09:00 -0600

    Made BlueModel, BlueView and BlueViewController files.

---
+++ sBlue/sBlue/sBlue/BlueModel.swift
@@ -0,0 +1,16 @@
+//
+//  BlueModel.swift
+//  sBlue
+//
+//  Created by Jose Canizares on 6/24/16.
+//  Copyright © 2016 Sinnply Blue. All rights reserved.
+//
+
+import Foundation
+
+struct BlueModel {
+    
+    enum Gestures {
+        
+    }
+}
---
+++ sBlue/sBlue/sBlue/BlueView.swift
@@ -0,0 +1,13 @@
+//
+//  BlueView.swift
+//  sBlue
+//
+//  Created by Jose Canizares on 6/24/16.
+//  Copyright © 2016 Sinnply Blue. All rights reserved.
+//
+
+import UIKit
+
+class BlueView: UIView {
+
+}
