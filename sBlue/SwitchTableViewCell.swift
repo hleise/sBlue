561b09d5ad8e23d7f896712e7cf05f3b7f1908a1
Hunter Leise <hleise27@gmail.com>
2016-07-21 19:13:58 -0600

    Added a few settings.

---
+++ sBlue/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  sBlue
+//
+//  Created by Hunter Leise on 7/21/16.
+//  Copyright © 2016 Vivo Applications. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
