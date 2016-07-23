1afa14a4dd6d69bcb719227b90eead3e72cbf374
Hunter Leise <hleise27@gmail.com>
2016-07-23 14:32:19 -0600

    Added the dropdown text and arrow in gestures.

---
+++ sBlue/dropdownViewController.swift
@@ -0,0 +1,42 @@
+//
+//  dropdownViewController.swift
+//  sBlue
+//
+//  Created by Hunter Leise on 7/23/16.
+//  Copyright © 2016 Vivo Applications. All rights reserved.
+//
+
+import UIKit
+
+class dropdownViewController: UIViewController {
+
+    @IBOutlet weak var leftSeparator: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        leftSeparator.frame.height
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
