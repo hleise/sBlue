9f9959bb92735a0862344b3400ae445dab9f5936
Hunter Leise <hleise27@gmail.com>
2016-07-23 16:34:05 -0600

    Basic UI without functionality is finished!

---
+++ sBlue/gesturesTableViewController.swift
@@ -0,0 +1,124 @@
+//
+//  gesturesTableViewController.swift
+//  sBlue
+//
+//  Created by Hunter Leise on 7/23/16.
+//  Copyright © 2016 Vivo Applications. All rights reserved.
+//
+
+import UIKit
+
+class gesturesTableViewController: UITableViewController {
+    @IBOutlet weak var gesturesNavigation: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return super.numberOfSectionsInTableView(tableView)
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return super.tableView(tableView, numberOfRowsInSection: section)
+    }
+
+    @IBAction func gesturesDropdownClicked(sender: UIButton) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        alert.addAction(UIAlertAction(
+            title: "My Gestures",
+            style: .Default)
+            { (action: UIAlertAction) -> Void in }
+        )
+        
+        alert.addAction(UIAlertAction(
+            title: "Default Gestures",
+            style: .Default)
+            { (action: UIAlertAction) -> Void in }
+        )
+        
+        alert.addAction(UIAlertAction(
+            title: "Cancel",
+            style: .Cancel)
+            { (action: UIAlertAction) -> Void in }
+        )
+        
+        alert.modalPresentationStyle = .Popover
+        let ppc = alert.popoverPresentationController
+        ppc?.sourceView = gesturesNavigation
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
